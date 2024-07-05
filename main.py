a82460a0fd355cb242ca6e4ba8b70d0a6d1d8f4b
Son <sson6926@gmail.com>
2024-07-05 11:57:02 +0700

    Add secret page

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello'
 
+@app.route('/secret')
+def secret_page():
+    return "This is secret page"
+
 if __name__ == '__main__':
     app.run(debug=True)
